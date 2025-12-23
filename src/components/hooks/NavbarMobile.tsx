8ae7cd69137857c1d5d1007a3b25d775efde3b6a
Rodrigo Opalo <r.opalo@icloud.com>
2025-12-23 15:03:48 -0500

    feat(seccion): Correccion de links registro e inicio

--- src/components/hooks/NavbarMobile.tsx
+++ src/components/hooks/NavbarMobile.tsx
@@ -144,7 +144,7 @@ export default function NavbarMobile() {
                 </li>
                 <li>
                   <a
-                    href="https://lem-box.com/Tracking/web/#/login"
+                    href="https://portal.lem-box.com/acceder"
                     target="_blank"
                     rel="noopener noreferrer"
                     onClick={() => setOpen(false)}
@@ -155,7 +155,7 @@ export default function NavbarMobile() {
                 </li>
                 <li>
                   <a
-                    href="https://lem-box.com/Tracking/web/#/register"
+                    href="https://portal.lem-box.com/registro"
                     target="_blank"
                     rel="noopener noreferrer"
                     onClick={() => setOpen(false)}
